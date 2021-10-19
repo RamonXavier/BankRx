e2798e59710806a6988557682516e15eba60c55d
GTIT\ramon.nascimento <ramon.nascimento@guiandotelecom.com.br>
2021-10-18 21:37:08 -0300

    finalizado menu em bottom

--- bankrx/lib/pages/home/widgets/item_menu_bottom.widget.dart
+++ bankrx/lib/pages/home/widgets/item_menu_bottom.widget.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ItemMenuBottom extends StatelessWidget {
-  const ItemMenuBottom({Key? key}) : super(key: key);
+  final IconData icon;
+  final String text;
+  const ItemMenuBottom({Key? key, required this.icon, required this.text})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
       child: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(7.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(icon),
+              Text(
+                text,
+                style: const TextStyle(fontSize: 10),
+              ),
+            ],
+          ),
+        ),
         width: 82,
         height: 30,
         decoration: BoxDecoration(
