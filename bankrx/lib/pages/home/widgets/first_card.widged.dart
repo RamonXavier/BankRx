040fccb6405f7a3bc229fa82116e3a68e4a6d0fe
GTIT\ramon.nascimento <ramon.nascimento@guiandotelecom.com.br>
2021-10-18 22:42:13 -0300

    primeiro card finalizado

--- bankrx/lib/pages/home/widgets/first_card.widged.dart
+++ bankrx/lib/pages/home/widgets/first_card.widged.dart
@@ -10,12 +10,79 @@ class FirstCard extends StatelessWidget {
       child: Column(
         children: [
           Expanded(
+            // ignore: avoid_unnecessary_containers
             child: Container(
               child: Row(
                 children: [
                   Expanded(
                     child: Column(
-                      children: [],
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Row(
+                            // ignore: prefer_const_literals_to_create_immutables
+                            children: [
+                              const Icon(
+                                Icons.credit_card,
+                                color: Colors.grey,
+                              ),
+                              const SizedBox(width: 5),
+                              const Text(
+                                "Cartão de crédito",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 14),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 20, top: 20, bottom: 20),
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                const Text(
+                                  "FATURA ATUAL",
+                                  style: TextStyle(
+                                      color: Colors.teal,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const Text.rich(
+                                  TextSpan(text: "R\$", children: [
+                                    TextSpan(
+                                        text: "1.230",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold)),
+                                    TextSpan(text: ",40"),
+                                  ]),
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                      color: Colors.teal, fontSize: 28),
+                                ),
+                                const Text.rich(
+                                  TextSpan(
+                                      text: "Limite disponível ",
+                                      children: [
+                                        TextSpan(
+                                            text: "R\$ 3.456,85",
+                                            style: TextStyle(
+                                                color: Colors.green,
+                                                fontWeight: FontWeight.bold)),
+                                      ]),
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 14),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                   Padding(
@@ -41,7 +108,7 @@ class FirstCard extends StatelessWidget {
                             ),
                             Expanded(
                               flex: 1,
-                              child: Container(color: Colors.teal),
+                              child: Container(color: Colors.green),
                             ),
                           ],
                         ),
@@ -57,6 +124,26 @@ class FirstCard extends StatelessWidget {
             flex: 1,
             child: Container(
               color: Colors.grey[200],
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Icon(
+                      Icons.shopping_cart,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(width: 4),
+                    const Text(
+                      "Compra mais recente no supermecado \n no valor de R\$ 124,67",
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                    const Icon(Icons.chevron_right_outlined,
+                        color: Colors.grey, size: 18)
+                  ],
+                ),
+              ),
             ),
           ),
         ],
